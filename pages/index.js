70db02a45c4e5587f5fe65e66bd5f37797260094
Alvaro <alvarucp@gmail.com>
2021-01-31 13:48:12 +0100

    feat: articles on home page

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,41 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Link from 'next/link';
+import fs from 'fs';
 
-export default function Home() {
+import styles from '../styles/Home.module.css';
+
+export default function Home({ articles }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>My Blog</title>
+        <title>My Tech Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h2 className={styles.description}>
-          My Latest Articles 📝
-        </h2>
-
+        <h2 className={styles.description}>My Latest Articles 📝</h2>
         <div className={styles.grid}>
-          <a href="" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="" className={styles.card}>
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a href="" className={styles.card}>
-            <h3>Deploy &rarr;</h3>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
+          {articles.map(article => (
+            <Link href={`/blog/${article}`} key={article}>
+              <a className={styles.card}>
+                <h3>{article.split('-').join(' ')} &rarr;</h3>
+                <p>Find in-depth information about this topic.</p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync('articles');
+  const articles = files.map(file => file.replace('.md', ''));
+
+  return {
+    props: {
+      articles
+    }
+  };
 }
